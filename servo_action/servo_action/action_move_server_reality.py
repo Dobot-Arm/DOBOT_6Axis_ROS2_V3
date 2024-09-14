5bfbfe46c5245499d2a9324ca11427bfbb7a652c
Dobot <88758937+Dobot-Arm@users.noreply.github.com>
2024-09-14 17:02:55 +0800

    Update action_move_server_reality.py

--- servo_action/servo_action/action_move_server_reality.py
+++ servo_action/servo_action/action_move_server_reality.py
@@ -6,9 +6,9 @@ import time
 
 import rclpy                                      # ROS2 Python接口库
 from rclpy.node   import Node                     # ROS2 节点类
-from rclpy.action import ActionServer             # ROS2 动作服务器类
-from control_msgs.action import FollowJointTrajectory  # 自定义的圆周运动接口
-from moveit_msgs.msg import DisplayTrajectory                 # ROS2标准定义的String消息
+from rclpy.action import ActionServer            
+from control_msgs.action import FollowJointTrajectory  
+from moveit_msgs.msg import DisplayTrajectory                 
 from trajectory_msgs.msg import JointTrajectoryPoint
 from dobot_msgs_v3.srv import *   # 自定义的服务接口                                   
 import time
@@ -69,7 +69,7 @@ class MoveCircleActionServer(Node):
 
 def main(args=None):                                       # ROS2节点主入口main函数
     rclpy.init(args=args)                                  # ROS2 Python接口初始化
-    node = MoveCircleActionServer("action_move_server")    # 创建ROS2节点对象并进行初始化
-    rclpy.spin(node)                                       # 循环等待ROS2退出
+    node = MoveCircleActionServer("action_move_server")   
+    rclpy.spin(node)                                       
     node.destroy_node()                                    # 销毁节点对象
     rclpy.shutdown()                                       # 关闭ROS2 Python接口
