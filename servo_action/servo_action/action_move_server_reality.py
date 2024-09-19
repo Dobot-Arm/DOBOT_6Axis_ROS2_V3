4e10115b4e7c4691be1a281b297c89fd824782ad
Dobot <88758937+Dobot-Arm@users.noreply.github.com>
2024-09-19 09:56:58 +0800

    Update action_move_server_reality.py

--- servo_action/servo_action/action_move_server_reality.py
+++ servo_action/servo_action/action_move_server_reality.py
@@ -6,11 +6,11 @@ import time
 
 import rclpy                                      # ROS2 Python接口库
 from rclpy.node   import Node                     # ROS2 节点类
-from rclpy.action import ActionServer            
+from rclpy.action import ActionServer             
 from control_msgs.action import FollowJointTrajectory  
 from moveit_msgs.msg import DisplayTrajectory                 
 from trajectory_msgs.msg import JointTrajectoryPoint
-from dobot_msgs_v3.srv import *   # 自定义的服务接口                                   
+from dobot_msgs_v3.srv import *                               
 import time
 import os
 
@@ -33,20 +33,21 @@ class MoveCircleActionServer(Node):
             self.get_logger().info('service not available, waiting again...') 
 
     def listener_callback(self, msg): 
-        self.adc = []                                              # 创建回调函数，执行收到话题消息后对数据的处理
-        for i in msg.trajectory[0].joint_trajectory.points:        # 输出日志信息，提示订阅收到的话题消息
+        self.adc = []                                              
+        for i in msg.trajectory[0].joint_trajectory.points:        
             acd = []
             for ii in i.positions:
                 acd.append(180*ii/3.14159)
             self.adc.append(acd)
+            print(f"Planning location:{acd}")
         self.Trajectory = True
             
 
-    def execute_callback(self, goal_handle):            # 执行收到动作目标之后的处理函数
+    def execute_callback(self, goal_handle):           
         if self.Trajectory:
             for i in self.adc:
                self.ServoJ_C(i[0],i[1],i[2],i[3],i[4],i[5])
-               print(i)
+               print(f"ServoJ:{i}")
                time.sleep(0.35)
             self.Trajectory = False
         else:
@@ -57,19 +58,19 @@ class MoveCircleActionServer(Node):
         return result                                 
     def ServoJ_C(self, j1, j2, j3, j4, j5,j6):  # 运动指令
             P1 = ServoJ.Request()
-            P1.a = float(j1)
-            P1.b = float(j2)
-            P1.c = float(j3)
-            P1.d = float(j4)
-            P1.e = float(j5)
-            P1.f = float(j6)
+            P1.j1 = float(j1)
+            P1.j2 = float(j2)
+            P1.j3 = float(j3)
+            P1.j4 = float(j4)
+            P1.j5 = float(j5)
+            P1.j6 = float(j6)
             P1.t = float(0.4)
             response = self.ServoJ_l.call_async(P1)
     
 
 def main(args=None):                                       # ROS2节点主入口main函数
-    rclpy.init(args=args)                                  # ROS2 Python接口初始化
+    rclpy.init(args=args)                                  
     node = MoveCircleActionServer("action_move_server")   
     rclpy.spin(node)                                       
-    node.destroy_node()                                    # 销毁节点对象
+    node.destroy_node()                                    
     rclpy.shutdown()                                       # 关闭ROS2 Python接口
